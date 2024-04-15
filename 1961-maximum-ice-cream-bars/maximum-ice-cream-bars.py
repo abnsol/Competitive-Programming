11e681730d43c7f0b143fde5cfc01a983926d979
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-04-15 11:01:07 +0300

    Time: 643 ms (45.18%) | Memory: 30.5 MB (51.24%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -4,13 +4,11 @@ class Solution:
         ans = 0
 
         for i in range(len(costs)):
-            if coins - costs[i] < 0:
+            if coins < costs[i]:
                 return ans
-            elif coins - costs[i] == 0:
-                return ans + 1
-            else:
-                coins -= costs[i]
-                ans += 1
+            
+            coins -= costs[i]
+            ans += 1
         
         return ans
 
