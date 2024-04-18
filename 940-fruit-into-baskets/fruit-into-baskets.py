47b5c88d0868d32fd74053f106b02a9568a03b46
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-04-19 02:38:29 +0300

    Time: 722 ms (20.60%) | Memory: 24.3 MB (23.84%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -7,12 +7,12 @@ class Solution:
             fruitBasket[fruit] += 1
 
             if len(fruitBasket) > 2:
-                startfruit = fruits[startIdx]
+                startfruit = fruits[startIdx]  # like the left pointer in SW
                 fruitBasket[startfruit] -= 1
 
-                if fruitBasket[startfruit] == 0:
+                if fruitBasket[startfruit] == 0: # delete the fruit from the dictionary
                     del fruitBasket[startfruit]
                 
-                startIdx += 1
+                startIdx += 1   # everytime left is increased while fruitBasket > 2 
         
-        return len(fruits) - startIdx
+        return len(fruits) - startIdx  # think of it as  
