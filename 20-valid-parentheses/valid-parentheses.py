ffede139facf2dcf7edc926d576396f51f0810d1
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-04-24 18:06:30 +0300

    Time: 40 ms (24.44%) | Memory: 16.6 MB (22.83%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        Brackets = {"()","{}","[]"} # Valid Pairs
         stack = []
+        valid = "()[]{}"
 
         for char in s:
-            if char in "({[":  # open Brackets
+            if char in "({[":
                 stack.append(char)
-            # if our stack is empty or last open bracket in our stack + the close bracket doesnt match
-            elif not stack or stack.pop() + char not in Brackets: 
-                return False
-                
-        # so when we finish stack must be empty = false 
-        return not stack 
+            else:
+                if stack and stack[-1] + char in valid:
+                    stack.pop()
+                else:
+                    return False
+        
+        if not stack: return True
+        return False
