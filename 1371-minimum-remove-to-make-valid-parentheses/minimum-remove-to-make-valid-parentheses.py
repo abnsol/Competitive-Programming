4e9710ef903a77ac67071a1d8873bc1fd2bae4f1
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-04-28 18:35:42 +0300

    Time: 1338 ms (5.03%) | Memory: 18.9 MB (25.69%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,26 +1,19 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # Initialize variables
-        openParenthesesCount = 0
-        arr = list(s)
+        stack = []
 
-        # First pass: mark excess closing parentheses with '*'
-        for i in range(len(arr)):
-            if arr[i] == '(':
-                openParenthesesCount += 1
-            elif arr[i] == ')':
-                if openParenthesesCount == 0:
-                    arr[i] = '*' # Mark excess closing parentheses
-                else:
-                    openParenthesesCount -= 1
+        for idx, c in enumerate(s):
+            if c not in "()":
+                continue
 
-        # Second pass: mark excess opening parentheses from the end
-        for i in range(len(arr) - 1, -1, -1):
-            if openParenthesesCount > 0 and arr[i] == '(':
-                arr[i] = '*' # Mark excess opening parentheses
-                openParenthesesCount -= 1
-        
-        # Filter out marked characters and construct the result string
-        result = ''.join(c for c in arr if c != '*')
+            if stack and stack[-1][1] + c == "()":
+                stack.pop()
+            else:
+                stack.append((idx,c))
 
-        return result
+        res = [] 
+        for idx, c in enumerate(s):
+            if (idx,c) not in stack:
+                res.append(c)
+
+        return "".join(res)
