e43e92aa08e7da2e53891315143d02846d9cf244
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-05-18 00:30:21 +0300

    Time: 288 ms (58.46%) | Memory: 32.1 MB (64.65%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,15 +5,25 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        dummy = head
-        while dummy is not None:
-            stack.append(dummy.val)
-            dummy = dummy.next
+        # find middle
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
         
-        second = head
-        while second is not None:
-            if second.val != stack.pop():
-                return False
-            second = second.next
+        # reverse after middle
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        # check palindrome
+        left, right = head, prev
+        while right:
+            if left.val != right.val: return False
+            left = left.next
+            right = right.next
+        
         return True
