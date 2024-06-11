bf64466a9c801f57e1c8061a2c8aad56aea9c52a
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-06-11 17:12:15 +0300

    Time: 53 ms (8.24%) | Memory: 16.9 MB (10.78%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,17 +1,15 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        idx1 = 0
+        count = Counter(arr1)
+        arr = []
+
         for num in arr2:
-            idx2 = idx1
-            while num in arr1[idx2:] and idx2 < len(arr1):
-                if arr1[idx2] == num:
-                    arr1[idx1], arr1[idx2] = arr1[idx2], arr1[idx1]
-                    idx1 += 1
-                idx2 += 1
+            arr += ([num] * count[num])
+            count[num] = 0
+        
+        count = sorted(count.items())
+        for rem in count:
+            arr += ([rem[0]] * rem[1])
+        
+        return arr
 
-        for i in range(idx2,len(arr1)):
-            for j in range(idx2 + 1,len(arr1)):
-                if arr1[j] < arr1[j - 1]:
-                    arr1[j - 1], arr1[j] = arr1[j], arr1[j - 1] 
-
-        return arr1
