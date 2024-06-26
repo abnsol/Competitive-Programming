b3e67b0064c30577f930c3e43bb6a4a0ab0b1d29
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-06-26 17:11:47 +0300

    Time: 280 ms (21.94%) | Memory: 24.7 MB (93.61%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -5,7 +5,6 @@ class Solution:
             while i != nums[i] - 1 and nums[i] <= n and nums[i] != nums[nums[i] - 1]:
                 nums[nums[i] - 1],nums[i] = nums[i], nums[nums[i] - 1]
         
-        print(nums)
         ans = []
         for i in range(n):
             if i != nums[i] - 1:
