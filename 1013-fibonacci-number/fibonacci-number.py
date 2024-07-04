83172fc71f33306979c1e3dbe5a7db22cda6f698
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-07-04 18:05:04 +0300

    Time: 38 ms (56.77%) | Memory: 16.5 MB (55.65%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,5 +1,15 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        maps = {0:0,1:1}
+        def recurse(n):
+            if n <= 1:
+                return n
+            
+            if n not in maps:
+                maps[n] = recurse(n - 1) + recurse(n - 2)
+                return maps[n]
+            else:
+                return maps[n]
+        
+        return recurse(n)
+            
