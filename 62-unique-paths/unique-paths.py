1b90c8eb2e4341a4c750b0c408234b2db0ced118
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-07-13 16:51:01 +0300

    Time: 28 ms (92.53%) | Memory: 16.5 MB (28.01%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,27 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0] * (n + 1) for _ in range(m + 1)]
+        grid[m - 1][n] = 1
+
+        for i in range(m - 1,-1,-1):
+            for j in range(n - 1,-1,-1):
+                grid[i][j] = grid[i + 1][j] + grid[i][j + 1]
         
-        dirs = [(0,1),(1,0)]
-        def inbound(i,j):
-            return i < m and j < n
-
-        memo = {}
-        def dfs(i,j):
-            if i == m - 1 and j == n - 1:
-                return 1
-            
-            if not inbound(i,j):
-                return 0
-            
-            if (i,j) in memo:
-                return memo[(i,j)]
-                
-            
-            path = 0
-            for r,c in dirs:
-                path += dfs(i + r,j + c)
-            
-            memo[(i,j)] = path
-            return path
-
-        return dfs(0,0)
+        return grid[0][0]
