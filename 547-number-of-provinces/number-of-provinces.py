c290bc833ec07f11a7a95e0afcc5b047375790f8
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-07-16 15:49:42 +0300

    Time: 183 ms (60.23%) | Memory: 17.3 MB (50.87%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,24 +1,41 @@
+class UnionFind:
+    def __init__(self,size):
+        self.parent = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self,child):
+        if child == self.parent[child]:
+            return child
+
+        self.parent[child] = self.find(self.parent[child])
+        return self.parent[child]
+    
+    def union(self,x,y):
+        parentx = self.find(x)
+        parenty = self.find(y)
+        if parentx != parenty:
+            if self.rank[parentx] > self.rank[parenty]:
+                self.parent[parenty] = parentx
+            elif self.rank[parenty] > self.rank[parentx]:
+                self.parent[parentx] = parenty
+            else:
+                self.parent[parentx] = parenty
+                self.rank[parenty] += 1
+    
+    def isconnected(self,x,y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        g = defaultdict(list)
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if isConnected[i][j] == 1:
-                    g[i].append(j)
+        n = len(isConnected)
+        components = n
+        union = UnionFind(n)
+
+        for i in range(n):
+            for j in range(i + 1,n):
+                if isConnected[i][j] and union.find(i) != union.find(j):
+                    components -= 1
+                    union.union(i,j)
         
-        visited = set()
-        cnt = 0
-        def dfs(node):
-            for i in g[node]:
-                if i not in visited:
-                    visited.add(i)
-                    dfs(i)
-        
-        for node in g:
-            if node not in visited:
-                cnt += 1
-                visited.add(node)
-                dfs(node)
-        
-        return cnt
-                
+        return components
+
