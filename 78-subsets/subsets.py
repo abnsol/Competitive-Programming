9bb37ab05e01c699918bde2b2e40f830baa1cb2f
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-07-18 16:19:15 +0300

    Time: 50 ms (5.99%) | Memory: 16.5 MB (99.34%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,20 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        subset = []
+        ans = []
         
-        def bt(idx):
-            if idx >= len(nums):
-                res.append(subset[:])
+        def bt(idx,arr):
+            if idx > len(nums):
                 return 
             
-            # takeit 
-            subset.append(nums[idx])
-            bt(idx + 1)
-
-            # leaveit
-            subset.pop()
-            bt(idx + 1)
-
-        bt(0)
-        return res
+            if arr not in ans:
+                ans.append(arr[:])
+            
+            for i in range(idx,len(nums)):
+                arr.append(nums[i])
+                bt(i + 1,arr)
+                arr.pop()
+        
+        
+        for i in range(len(nums)):
+            bt(i,[])
+        return ans
