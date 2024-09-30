8f1d422c30ac79e675cd325ecd9798c22e5cd763
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-09-30 15:42:47 +0300

    Time: 654 ms (25.60%) | Memory: 30.5 MB (48.81%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -2,19 +2,10 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         costs.sort()
         i = ans = 0
-        
+
         while i < len(costs) and (coins - costs[i]) >= 0:
             coins -= costs[i]
             ans += 1
             i += 1
         
         return ans
-
-
-
-'''
-1 sort
-2 while coins > 0 and inbound increase the num
-[2,3,3,5,6,6,6,7,9,10]
-
-'''
