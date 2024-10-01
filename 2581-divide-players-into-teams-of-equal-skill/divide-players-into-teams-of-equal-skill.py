1f2d348f511a666036b9f4c441e59da01f29aaf1
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-10-01 10:20:13 +0300

    Time: 393 ms (77.84%) | Memory: 29.4 MB (66.85%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,16 +1,35 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
         skill.sort()
-        left , right = 0, len(skill) - 1
-        equals =  skill[left] + skill[right]
-        products = []
-        while left < right:
-            temp = skill[left] + skill[right]
-            if temp != equals:
+        requiredSkill = skill[0] + skill[-1]
+        totalChemistry = skill[0] * skill[-1]
+        l, r = 1, len(skill) - 2
+
+        while l < r:
+            if skill[l] + skill[r] != requiredSkill:
                 return -1
-            else:
-                products.append(skill[left] * skill[right])
-                left += 1
-                right -= 1
-        return sum(products)
-        
+
+            totalChemistry += skill[l] * skill[r]
+            l += 1
+            r -= 1
+
+        return totalChemistry
+
+
+'''
+Two pointers approach
+ sort
+ intialize my pointers
+ iterate through the array
+ previous skill = 6
+ totalchemistry = 0
+ check if sum of skills is equal with previous skill sum
+        not : return  -1
+        is: continue
+            increment tc by product of team skill
+ 
+ return total chemistry here
+
+1 2 3 3 4 5 
+
+'''
