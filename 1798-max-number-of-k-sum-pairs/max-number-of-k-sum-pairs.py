7adc66c5f5c80c245aaabc05de1d5c1670ffd837
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-10-01 10:32:07 +0300

    Time: 513 ms (20.89%) | Memory: 28.6 MB (91.81%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -1,19 +1,33 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        left, right = 0, len(nums) - 1
-        operations = 0
+        l, r = 0, len(nums) - 1
+        numberOfOps = 0
 
-        while left < right:
-            temp = nums[right] + nums[left]
-            if temp == k:
-                operations += 1
-                left += 1
-                right -= 1
-            elif temp > k:
-                right -= 1
+        while l < r:
+            if nums[l] + nums[r] > k:
+                r -= 1
+            elif nums[l] + nums[r] < k:
+                l += 1
             else:
-                left += 1
-    
-        return operations 
+                numberOfOps += 1
+                l += 1
+                r -= 1
+        
+        return numberOfOps
 
+
+'''
+Two pointers approach
+
+sort
+intialize pointers
+create numberofoperation = 0
+iterating while l < r
+ check if sum != target(k)
+    increase accly
+ is
+    increase the numberofoperations by one 
+    chnge both pointers
+return numberof operation
+'''
