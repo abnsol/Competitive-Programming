3d094ed08ffe50c8c9702a542b127c461ab49eaf
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-26 20:41:06 +0300

    Time: 7 ms (59.10%) | Memory: 18.7 MB (7.15%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.py
@@ -0,0 +1,35 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        n = len(nums) - 1
+        ps = [0]
+        ss = [0]
+
+        for i in range(n):
+            ps.append(nums[i] + ps[i])
+            ss.append(nums[n - i] + ss[i])
+        
+        print(ps)
+        ss.reverse()
+        print(ss)
+        for i in range(n + 1):
+            if ps[i] == ss[i]:
+                return i
+        
+        return -1
+
+'''
+[1,7,3,6,5,6]
+[0, 1, 8, 11,17,22]
+[27,20,17,11, 6,0]
+
+[2,1,-1]
+[0,2,3]
+[0,]
+
+intialize psa and ssa = 0
+find ps starting with 0
+find ss starting with 0
+
+loop through the ps and ss to check for the pivot idx
+return idx
+'''
