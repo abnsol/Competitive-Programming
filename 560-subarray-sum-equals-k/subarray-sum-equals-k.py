4b8fb96354619ce641b45ab8de16c398534dcb7c
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-27 13:11:30 +0300

    Time: 47 ms (10.80%) | Memory: 19.9 MB (17.68%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,28 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # variant of 974
         cnt = Counter({0 : 1})
-        ans = ttl = 0
+        acc = 0
+        res = 0
 
         for num in nums:
-            ttl += num
-            ans += cnt[ttl - k]  # is the ttl - k in cnt 
-            cnt[ttl] += 1        # 
-        
-        return ans
-            
+            acc += num
+            res += cnt[acc - k]
+            cnt[acc] += 1
 
-         
+        return res            
+
+'''
+0 > 1
+1 > 1
+2 > 1
+
+
+acc = 1
+res = 0
+
+acc = 2
+res = 1
+
+acc = 3
+res = 2
+'''
