09e29ec6cdac6cf011eb0522a1a2e73c7d49d827
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-28 17:13:09 +0300

    Time: 3 ms (67.87%) | Memory: 17.6 MB (13.41%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,15 +1,15 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # range sum query
-        queries = [0] * 1001
+        rangeUpdate = [0] * (1001)
 
-        for nump,fromi,to in trips:
-            queries[fromi] += nump
-            queries[to] -= nump
-
-        for i in range(1,1001):
-            queries[i] += queries[i - 1]
-            if queries[i] > capacity or queries[i - 1] > capacity:
-                return False
+        for passanger,start,to in trips:
+            rangeUpdate[start] += passanger
+            rangeUpdate[to] -= passanger
         
-        return True
+        acc = 0
+        for num in rangeUpdate:
+            if acc > capacity:
+                return False
+            acc += num
+
+        return True    
