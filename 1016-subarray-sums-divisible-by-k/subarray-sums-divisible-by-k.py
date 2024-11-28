1883bad0366c5ca99ca8ebb080ac7341527cfbc7
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-28 14:50:54 +0300

    Time: 183 ms (5.06%) | Memory: 21.2 MB (40.95%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,23 +1,25 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        # pigeonhole principle
-        cnt = Counter({0 : 1})   # keep track of the remainders , empty subarray is divisible by k
-        ans = ttl = 0            # answer and running sum 
+        cnt = Counter({0 : 1})
+        ttl = 0
+        res = 0
 
-        for num in nums:
-            ttl += num             
-            ans += cnt[ttl % k]   # wiz that running sum it can make cnt[ttl % k] subarrays of sum divisible by k 
+        for i in nums:
+            ttl += i
+            res += cnt[ttl % k]
             cnt[ttl % k] += 1
+            print(ttl % k," ",cnt[ttl % k])
         
-        return ans
+        return res
+
+'''
+
+brute force
+find every subarray
+find the sum of each subarray and check if div by k
+increment result
+O(n3)
+
+prefix sum approach
+'''
         
-# i            j
-# [0,4,9,0,7,4,5]
-# (ps[j] - ps[i]) % k == 0  --> ps[i] == ps[j] --- (*)
-# what if % k != 0
-# ps[j] % k = R1 --> ps[j] = A * k + R1      
-# ps[i] % k = R2 --> ps[i] = B * K + R2
-# ps[j] - ps[i] = A * k + R1 - (B * k + R2)
-#               = (A - B) * k + (R1 - R2)  
-#               = (A - B) * k               assume R1 = R2
-# so a property to notice here if R1 == R2 then prefix sums can be divisible by k
