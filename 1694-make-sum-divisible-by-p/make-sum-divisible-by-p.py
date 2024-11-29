a147b661c5bcf23f4a2d3e029636eb92d701f52f
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-29 16:12:14 +0300

    Time: 78 ms (63.08%) | Memory: 37.7 MB (6.99%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -1,13 +1,29 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        if sum(nums)%p==0:
-            return 0
-        target = sum(nums) % p
-        dic,n = {0:-1},len(nums)
-        cur,ret = 0,n
-        for i,num in enumerate(nums):
-            cur = (cur+num)%p
-            if dic.get((cur-target)%p) is not None:
-                ret = min(ret,i-dic.get((cur-target)%p))
-            dic[cur] = i
-        return ret if ret < n else -1
+        ttl = sum(nums)
+        # prefix[i] % p == prefix[j] % p
+        remainder = ttl % p
+
+        if remainder == 0:
+            return 0 
+
+        res = len(nums)
+        cur_sum = 0
+        # save rem mod to where they appear last 
+        # looking for this to remove from arr to make sum div by p
+        remainderMod = {0 : -1} 
+
+        for idx,num in enumerate(nums):
+            cur_sum = (cur_sum + num) % p
+            # target = cur_sum - remainder
+            # this is the maths to find the number we want to remove from the 
+            # current sum remainder to make the remaining sum div by p
+            prefix = (cur_sum - remainder + p) % p
+            if prefix in remainderMod:
+                length = idx - remainderMod[prefix] # length of subarray
+                res = min(length,res)
+            
+            remainderMod[cur_sum] = idx
+
+        return -1 if res == len(nums) else res 
+
