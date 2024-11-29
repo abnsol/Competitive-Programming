41cd37c0ead9eee4282ed7d48dbf7ec17f1d8f38
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-29 17:26:58 +0300

    Time: 47 ms (92.82%) | Memory: 37.5 MB (10.31%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,17 +1,36 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        remainder = {0 : -1}
         ttl = 0
+        remMod = { 0 : -1 }
 
-        for i,n in enumerate(nums):
-            ttl += n
-            r = ttl % k
-            if r not in remainder:
-                remainder[r] = i
-            elif i - remainder[r] > 1:
-                return True 
-            
+        for idx,num in enumerate(nums):
+            ttl += num
+            remainder = ttl % k
+            if remainder not in remMod:
+                remMod[remainder] = idx
+            elif idx - remMod[remainder] > 1:
+                return True
+                
+        
         return False
+        
 '''
 23 2 4 6 7
+bruteforce
+O(n2)
+optimize bf
+[]
+(prefix[j] - prefix[i]) % k == 0
+j > i
+sum elements b/n j and i is divisible by 6
+[0,23,25,29,35,42] 
+optimize ps
+prefix[j] % k == prefix[i] % k
+hashmap => remMod : idx
+idx - hashMap[prefix[j] % k] > 1
+{
+    0 : -1
+    5 :  0
+    1 :  1
+}
 '''
