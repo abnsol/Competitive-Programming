849d57a8869bd83f66ac36882fc10317fc47a8a8
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-29 21:59:05 +0300

    Time: 79 ms (93.13%) | Memory: 44.2 MB (61.90%) - LeetSync

--- 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
+++ 1695-maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.py
@@ -1,23 +1,31 @@
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], req: List[List[int]]) -> int:
-        n=len(nums)
-        r=len(req)
-        a=[0 for i in range(n)]    
-        for i in range(r):
-            a[req[i][0]]+=1
-            if(req[i][1]<n-1):
-                a[req[i][1]+1]-=1
+        n = len(nums)
+        mod = 10 ** 9 + 7
+        rangeUpdate = [0] * (n + 1)
+        for start,end in req:
+            rangeUpdate[start] += 1
+            rangeUpdate[end + 1] -= 1
         
+        acc = 0
         for i in range(n):
-            if(i==0):
-                continue
-            a[i]+=a[i-1]
-
-        a.sort()
+            acc += rangeUpdate[i]
+            rangeUpdate[i] = acc
+        
         nums.sort()
-        ans=0
-        N=1e9 + 7
-        for i in range(n-1,-1,-1):
-            ans+=(a[i]*nums[i])%N
-            ans%=N
-        return int(ans%N)
+        last_element = rangeUpdate[-1]
+        rangeUpdate = sorted(rangeUpdate[:-1]) + [last_element]
+
+        ans = 0
+        for i in range(n):
+            ans = (ans + nums[i] * rangeUpdate[i]) % mod
+        
+        return ans
+
+
+'''
+bruteforce => O(n3)
+prefix Sum approach
+[1,3,6,10,15]
+sum(1,3) = ps[end] - ps[start]
+'''
