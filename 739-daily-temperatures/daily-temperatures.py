b8b5be52a76e0144aa740548a4c15a239c0d19e1
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-11-30 19:03:24 +0300

    Time: 135 ms (22.79%) | Memory: 34.7 MB (5.11%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,20 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # if temp less than the previous add to stack
-        # when a greater temp comes pop and put it in the right position
         n = len(temperatures)
+        stk = [] # temp,idx
         ans = [0] * n
-        stack = []
-
-        for idx, temp in enumerate(temperatures): # use enumerate!
-            while stack and temp > stack[-1][0]:
-                t , i  = stack.pop()
-                ans[i] = idx - i      # number of less tempratures
+        for idx,temp in enumerate(temperatures):
+            while stk and temp > stk[-1][0]:
+                poppedTemp,poppedIdx = stk.pop()
+                ans[poppedIdx] = idx - poppedIdx
             
-            stack.append((temp,idx))
-        
+            stk.append([temp,idx])
+
         return ans
 
-
-            
-            
+        
