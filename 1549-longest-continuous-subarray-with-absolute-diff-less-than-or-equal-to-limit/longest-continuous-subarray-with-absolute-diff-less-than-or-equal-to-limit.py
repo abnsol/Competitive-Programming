52d799ade03136418bfa0072a0967878c1c5d54f
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-02 14:52:41 +0300

    Time: 193 ms (81.52%) | Memory: 25.7 MB (66.19%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,28 +1,26 @@
 class Solution:
-    def longestSubarray(self, nums, limit):
-        max_heap = []
-        min_heap = []
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        max_q = deque()
+        min_q = deque()
+        l = res = 0
 
-        left = 0
-        max_length = 0
-
-        for right in range(len(nums)):
-            heapq.heappush(max_heap, (-nums[right], right))
-            heapq.heappush(min_heap, (nums[right], right))
-
-            # Check if the absolute difference between the maximum and minimum values in the current window exceeds the limit
-            while -max_heap[0][0] - min_heap[0][0] > limit:
-                # Move the left pointer to the right until the condition is satisfied.
-                # This ensures we remove the element causing the violation
-                left = min(max_heap[0][1], min_heap[0][1]) + 1
-
-                # Remove elements from the heaps that are outside the current window
-                while max_heap[0][1] < left:
-                    heapq.heappop(max_heap)
-                while min_heap[0][1] < left:
-                    heapq.heappop(min_heap)
-
-            # Update max_length with the length of the current valid window
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
+        for r in range(len(nums)):
+            while min_q and min_q[-1] > nums[r]:
+                min_q.pop()
+            while max_q and max_q[-1] < nums[r]:
+                max_q.pop()
+            
+            max_q.append(nums[r])
+            min_q.append(nums[r])
+            
+            while max_q[0] - min_q[0] > limit:
+                if max_q[0] == nums[l]:
+                    max_q.popleft()
+                if min_q[0] == nums[l]:
+                    min_q.popleft()
+    
+                l += 1
+            
+            res = max(res,r - l + 1)
+        
+        return res
