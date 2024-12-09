0222bd324ffb469ce170049c3f5f149114cf6ae4
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-09 20:56:37 +0300

    Time: 0 ms (100.00%) | Memory: 22.5 MB (27.12%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = max(citations)
+        cnt_srt = [0] * (n + 1)
+
+        for cite in citations:
+            cnt_srt[cite] += 1
+        
+        numbersAfter = 0
+        for i in range(n,-1,-1):
+            numbersAfter += cnt_srt[i]
+            if numbersAfter >= i:
+                return i
+        
+        return 0
