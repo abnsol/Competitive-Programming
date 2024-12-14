0c4ecd1affa781ef958e8c760290858f57442778
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-14 15:23:07 +0300

    Time: 18 ms (71.61%) | Memory: 17.6 MB (8.78%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,10 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        maps1 = Counter(s1)
-        for i in range(len(s2) - len(s1) + 1):
-            if maps1 == Counter(s2[i:i + len(s1)]):
-                return True
+        m = len(s1)
+        n = len(s2)
+        l = 0
+
+        if m > n: return False
+
+        cnt = Counter(s1)
+        cntS2 = {}
+
+        for _ in range(m):
+            cntS2[s2[_]] = cntS2.get(s2[_],0) + 1
+        
+        if cntS2 == cnt: return True
+
+        for r in range(m,n):
+            cntS2[s2[r]] = cntS2.get(s2[r],0) + 1
+            cntS2[s2[l]] -= 1
+            if not cntS2[s2[l]]: del cntS2[s2[l]]
+            l += 1
+            if cntS2 == cnt: return True
+        
         return False
 
-        
 
+
+
+
+'''
+s1.len > s2.len return false
+k = s1.len
+fixed Sw approach
+
+'''
+
+        
