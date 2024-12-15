4d1bf3edfbd055a5e31d4f66f48f001080b6b158
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-15 16:39:39 +0300

    Time: 129 ms (65.51%) | Memory: 17.9 MB (5.23%) - LeetSync

--- 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
+++ 1351-replace-the-substring-for-balanced-string/replace-the-substring-for-balanced-string.py
@@ -1,66 +1,42 @@
 class Solution:
     def balancedString(self, s: str) -> int:
         n = len(s)
+        print(n)
         cnt = Counter(s)
-
+        print(cnt)
         removeCnt = Counter()
-        # find the excess elements
-        for key in cnt:
-            if cnt[key] > n / 4:
-                removeCnt[key] = cnt[key] - n//4
+
+        for keys in cnt:
+            if cnt[keys] > n/4:
+                removeCnt[keys] = cnt[keys] - n//4
         
-        if not removeCnt: return 0 
-        ans = n
+        if not removeCnt: return 0
+        res = n
         l = 0
+
         for r in range(n):
             if s[r] in removeCnt:
                 removeCnt[s[r]] -= 1
             
             while max(removeCnt.values()) <= 0:
-                ans = min(ans, r - l + 1)
+                res = min(res,r - l + 1)
                 if s[l] in removeCnt:
                     removeCnt[s[l]] += 1
                 l += 1
-            
-        return ans
-            
+        
+        return res
+
 
 '''
-(curCount['W'] >= removeCnt['W'] \
-            or curCount['Q'] >= removeCnt['Q'] or \
-            curCount['E'] >= removeCnt['E'] or \
-            curCount['R'] >= removeCnt["R"])
-sliding window
-{
-    Q : 2
-    W : 1
-    E : 1
-    R : 0
-}
-
-R => 1
-Q => 1
-count the number of unique items and 
-QQQWERQQQWWEQWEW
-Q : 6 => 2
-W : 5 => 1
-E : 4 
-R : 1
-=> 4
-QRREERQQQWWEQWEW
-
-w : 8 => 3
-E : 4 => 
-Q : 5 => 
-R : 3 => 
-"WWEQERQWQWWRWWERQWEQ"
-"WWEQERQRREQRWWERQWEQ"
-"RREQERQEQWWRWWERQWEQ"
-
-"WQWRQQQW"
-w : 3 => 1
-Q : 4 => 2
-R : 1 => 
-E : 0 => 
-
+every letter must appear n/4 times
+where are those extra letters we dont care about the none extra ones
+QQWQQRRRWWQQ
+w,r
+Q => 3
+EEWE
+cnt = count s
+countExtras = 3
+add => subtract count Extras if letter[r] is extra
+check if count extras is 0 => update our result which is going to be the min
+remove => l elemetn while check is 0
 '''
