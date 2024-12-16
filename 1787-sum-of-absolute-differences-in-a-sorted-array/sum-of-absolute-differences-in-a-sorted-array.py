7a5db9915c69a2e390cbf0b66ffe37baa22b2e26
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-16 16:56:05 +0300

    Time: 654 ms (21.58%) | Memory: 33 MB (14.56%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.py
@@ -1,33 +1,31 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
-        # main intuition is we have to find the sum 
-        # before the element and after the element
-        # independently and dont forget to multiply the number
-        # to the amount of number before and after it
+        pre = []
+        suf = []
+        acc = 0
+        sufAcc = 0
         n = len(nums)
-        prefix = [0]
-        suffix = [0]
 
-        for i in range(n - 1):
-            prefix.append(prefix[i] + nums[i])
-            suffix.append(suffix[i] + nums[n - 1 - i])
-        
-        suffix.reverse()
+        for i in range(n):
+            pre.append(acc)
+            suf.append(sufAcc)
+            acc += nums[i]
+            sufAcc += nums[n - i - 1]
+       
+        suf.sort(reverse=True)
         ans = []
         for i in range(n):
-            res = (nums[i] * i) - prefix[i] \
-            + abs(nums[i] * (n - 1 - i) - suffix[i])
+            res = 0
+            res += nums[i] * i - pre[i]
+            res += abs(suf[i] - nums[i] * (n - i - 1))
             ans.append(res)
-        
+    
         return ans
 
 
 
 '''
-[1,4,6,8,10]
-[0,1,5,11,19]
-[28,24,18,10,0]
-(1 * 0) - 0  + |1 * (5 - 0 - 1) - 28|  = 24
-(4 * 1) - 1 + |4 * (5 - 1 - 1) - 24| = 3 + 12 = 15
-(6 * 2) - 5 + |6 * (5 - 2 - 1) - 18| = 7 + 6 = 13
+[2,1,2]
+[0,2,5]
+[8,5,0]
 '''
