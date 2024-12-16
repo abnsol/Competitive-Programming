8c02e325fc92aab3f5520f618ae619f44c00ef62
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-16 22:37:38 +0300

    Time: 7 ms (81.29%) | Memory: 17.5 MB (50.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,34 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l ,r = 0, len(s) - 1
+        n = len(s)
+        l = 0
+        r = n - 1
 
         while l < r:
-            while (not s[l].isalnum()) and l < r:
+            if not s[l].isalnum():
                 l += 1
-            while (not s[r].isalnum()) and l < r:
+            elif not s[r].isalnum():
                 r -= 1
-            
-            if l > r:
-                return False
-            
-            if s[l].lower() != s[r].lower():
-                return False
-            
-            l += 1
-            r -= 1
-
+            else:
+                if s[l].lower() != s[r].lower():
+                    return False
+                else:
+                    l += 1
+                    r -= 1
+        
         return True
-
-'''
-Two pointers approach
-
-intialize my pointers
-while left pointer lessthan right pointer
-    check alphanumeric or not
-        not: increase pointer accordingly
-        is: check if left and right are the same (must be lower)
-            not return false
-            if increase accly
-
-return true passed all the conditions
-'''
+        
