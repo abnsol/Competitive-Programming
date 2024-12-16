33109a53da86e881cd06e8f9c173fee9b968bb49
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-16 18:32:54 +0300

    Time: 44 ms (9.87%) | Memory: 20.9 MB (12.89%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,27 +1,20 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        cnt = Counter({0 : 1})
-        ttl = 0
-        res = 0
+        ttl = res = 0
+        rem_to_cnt = Counter({0 : 1})
 
-        for i in nums:
-            ttl += i
-            res += cnt[ttl % k]
-            cnt[ttl % k] += 1
+        for num in nums:
+            ttl += num
+            res += rem_to_cnt[ttl % k]
+            rem_to_cnt[ttl % k] += 1
         
         return res
+    
 
-'''
-brute force
-find every subarray
-find the sum of each subarray and check if div by k
-increment result
-O(n3)
 
-[0,4,9,9,7,4,5] O(n2)
-(prefix[j] - prefix[i]) % k == 0
+"""
+prefix[i - 1] % k = prefix[j] % k
+j  > i 
+rem_to_cnt
 
-prefix sum approach
-prefix[j] % k == prefix[i] % k
-'''
-        
+"""
