303ba00eb8f192702dc310999caa93f2a47e483c
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-16 18:01:09 +0300

    Time: 49 ms (7.46%) | Memory: 19.9 MB (23.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,32 +1,21 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        acc = 0
-        count = {0 : 1} # store the previous results that can be subrtracted
+        cnt = Counter({0:1})
         res = 0
+        ttl = 0
 
         for num in nums:
-            acc += num
-            if acc - k in count:
-                res += count[acc - k]
-            
-            count[acc] = count.get(acc,0) + 1
+            ttl += num
+            res += cnt[ttl - k]
+            cnt[ttl] += 1
+        
+        return res
 
-        return res 
+
 
 '''
-brute force approach
-find every sub array
-find the sum of those sub arrays
-check equal with k
-
-optimized bf
-ps = [0,1,2,3]
-
-O(n) => prefixsum approach with hashmap
-ps = [0,1,2,3]
-res = 0
-0 : 1
-1 : 1
-2 : 1 res = 1
-3 : 1 res = 2
+[0,1,2,3]
+hashmap = {}
+2 - x = k
+x = 2 - k
 '''
