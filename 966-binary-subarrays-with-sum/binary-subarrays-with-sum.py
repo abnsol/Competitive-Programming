25634ff2acaaded71bef4caa422e6db0d5a6e052
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-17 09:04:49 +0300

    Time: 29 ms (67.59%) | Memory: 21 MB (11.15%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,42 +1,18 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        hashMap = {0 : 1}
-        runningSum = 0
+        ps = {0 : 1}
+        ttl = 0
         res = 0
 
         for num in nums:
-            runningSum += num
-            if runningSum - goal in hashMap:
-                res += hashMap[runningSum - goal]
-
-            hashMap[runningSum] = hashMap.get(runningSum,0) + 1
+            ttl += num
+            if ttl - goal in ps:
+                res += ps[ttl - goal]
+            ps[ttl] = ps.get(ttl,0) + 1 
         
         return res
-
         
-
 '''
-bf approach
-O(n3)
-optimized bf
-O(n2)
-[0,1,1,2,2,3]
-j > i
-ps[j] - ps[i] = goal
-very optimized bf
-O(N)
-acc = 3
-res = 4
-{
-    0 : 1,
-    1 : 2,
-    2 : 2,
-    3 : 1
-}
-
-acc = 0
-res = 15
-{
-    0 : 6,
-}
+x - ps = goal
+x - goal = ps
 '''
