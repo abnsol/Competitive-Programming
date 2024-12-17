a0f5d7f839c545fa00ddfeeebdcb123e374b71e7
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2024-12-17 11:47:34 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (5.73%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,16 +1,12 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        # reverse process --> start from sorted then get the deck order
-        deck.sort() # start by sorting deck
-        n = len(deck)
-        res = [0] * n
-        q = deque(range(0,n)) # indexes
+        deck.sort()
+        res = [0]* len(deck)
+        q = deque(range(len(deck)))
 
-        for cards in deck:
-            i = q.popleft()   # pop index where we insert the popped card (tho not literally poping)
-            res[i] = cards
-
+        for n in deck:
+            res[q.popleft()] = n
             if q:
-                q.append(q.popleft()) # move the next index to the last
-        
-        return res
+                q.append(q.popleft())
+
+        return res 
