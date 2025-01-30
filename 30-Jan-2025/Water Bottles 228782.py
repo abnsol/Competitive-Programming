28af4360a97ec4a2379bbd225753cf7b3419f25b
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-01-30 23:05:25 +0300

    Created 'Water Bottles' by A2SV Hub

---
+++ 30-Jan-2025/Water Bottles 228782.py
@@ -0,0 +1,13 @@
+# Problem: Water Bottles - https://leetcode.com/problems/water-bottles/description
+
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = numBottles
+        rem = 0
+        while numBottles >= numExchange:
+            ans += (numBottles // numExchange)
+            numBottles = numBottles % numExchange + numBottles // numExchange
+        
+        return ans
+
+        
