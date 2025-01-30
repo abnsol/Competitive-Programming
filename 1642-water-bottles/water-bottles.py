08f92207ad5b6f592c95b5b49a065cfde54898ed
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-01-30 18:01:00 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (20.04%) - LeetSync

--- 1642-water-bottles/water-bottles.py
+++ 1642-water-bottles/water-bottles.py
@@ -1,14 +1,11 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        consumed_bottles = 0
-
+        ans = numBottles
+        rem = 0
         while numBottles >= numExchange:
-            # Consume numExchange full bottles.
-            consumed_bottles += numExchange
-            numBottles -= numExchange
+            ans += (numBottles // numExchange)
+            numBottles = numBottles % numExchange + numBottles // numExchange
+        
+        return ans
 
-            # Exchange them for one full bottle.
-            numBottles += 1
-
-        # Consume the remaining numBottles (less than numExchange).
-        return consumed_bottles + numBottles
+        
