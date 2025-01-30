a5f0a3961bc701e7a5b0fce85f86e2826fb19513
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-01-30 15:28:25 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 30-Jan-2025/Longest Common Prefix 226634.py
@@ -0,0 +1,28 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs: 
+            return ""
+
+        ans = strs[0]
+        n = len(strs)
+        for i in range(1,n):
+            prefix = ""
+            j = k = 0
+            while j < len(ans) and k < len(strs[i]):
+                if ans[j] == strs[i][k]:
+                    prefix += ans[j]
+                    j += 1
+                    k += 1
+                else:
+                    break
+
+            ans = prefix
+        
+        return ans
+
+
+
+
+        
