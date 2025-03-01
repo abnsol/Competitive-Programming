038c7cf9f2b4f3029668f3bd0e7a70f69ad45672
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-03-01 21:36:31 +0300

    Created 'C - Binary Flip' by A2SV Hub

---
+++ A2SV G6 - Round #4 01-Mar-2025/C - Binary Flip 287771.py
@@ -0,0 +1,40 @@
+# Problem: C - Binary Flip - https://codeforces.com/gym/590053/problem/C
+
+import sys
+
+def solve():
+    n = int(input())
+    A = input()
+    B = input()
+    
+    A += '0'
+    B += '0'
+    count = 0
+    
+    for i in range(n):
+        # If count == 0, it means we have equal 1s and 0s
+        if A[i] == '1':
+            count += 1
+        else:
+            count -= 1
+        
+        if count != 0:
+            if A[i] != B[i] and A[i+1] == B[i+1]:
+                print("NO")
+                return
+            
+            elif A[i] == B[i] and A[i+1] != B[i+1]:
+                print("NO")
+                return
+        
+        # Alternative way of writing the above condition
+        # if ((A[i] == B[i]) != (A[i + 1] == B[i + 1])) and count != 0:
+        #     print("NO")
+        #     return
+    
+    print("YES")
+
+
+t = int(input())
+for _ in range(t):
+    solve()
