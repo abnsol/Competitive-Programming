ca4fecdb62a79f007ac558f22b5079d69e1ea178
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-03-12 17:12:11 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 12-Mar-2025/E - Minimum Subsequence 303941.py
@@ -0,0 +1,44 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input()))
+
+    zero_stk = [] 
+    one_stk = []
+    ans = [0] * (n)
+    sub = 0
+    for idx,num in enumerate(a):
+        if num == 0:
+            if one_stk:
+                cur_sub = one_stk.pop()
+            else:
+                sub += 1
+                cur_sub = sub
+            zero_stk.append(cur_sub)
+        else:
+            if zero_stk:
+                cur_sub = zero_stk.pop()
+            else:
+                sub += 1
+                cur_sub = sub
+            one_stk.append(cur_sub)
+        ans[idx] = cur_sub
+
+
+    print(sub) 
+    print(*ans)
+
+
+
+
+
+
+    
+
+
+
+    # print(cnt)   
+    # print(*ans)
+
