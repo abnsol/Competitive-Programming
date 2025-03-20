25e897b362fcc51d1f1d9faf84966dbb76a837e1
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-03-20 22:42:14 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 20-Mar-2025/Permutations 313728.py
@@ -0,0 +1,23 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def bt(ans,lis):
+            if len(ans) == len(nums):
+                res.append(ans[:])
+                return
+            
+            for i in range(len(lis)):
+                ans.append(lis[i])
+                bt(ans,lis[:i] + lis[i + 1:])
+                ans.pop()
+            
+        bt([],nums)
+        return res
+
+            
+
+            
+
+        
