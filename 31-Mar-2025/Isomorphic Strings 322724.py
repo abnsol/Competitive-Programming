fe4c271ca2079542f0bf838bd85bc3f97bcebf89
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-03-31 20:18:01 +0300

    Created 'Isomorphic Strings' by A2SV Hub

---
+++ 31-Mar-2025/Isomorphic Strings 322724.py
@@ -0,0 +1,11 @@
+# Problem: Isomorphic Strings - https://leetcode.com/problems/isomorphic-strings/
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mapST ,mapTS = {} , {}
+        for c, d  in zip(s,t):
+            if ((c in mapST and  mapST[c] != d) or (d in mapTS and mapTS[d] != c)):
+                return False
+            mapST[c] = d
+            mapTS[d] = c
+        return True
