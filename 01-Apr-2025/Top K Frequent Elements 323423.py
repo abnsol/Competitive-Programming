53ae9cb28d689b1b1af9d4936e2a60abea7a9ecc
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-04-01 19:36:04 +0300

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 01-Apr-2025/Top K Frequent Elements 323423.py
@@ -0,0 +1,21 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        for num,count in counter.items():
+            bucket[count] += [num]
+            
+        ans = []
+        for i in range(len(nums),-1,-1):
+            if k != len(ans):
+                if bucket[i] != []:
+                    ans += bucket[i]
+            else:
+                return ans
+            
+        return ans
+
+
