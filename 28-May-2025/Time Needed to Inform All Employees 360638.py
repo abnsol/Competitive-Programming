5215855a5bbd3bf3c492c0a92c3ddf1eb0f877dc
Abenezer Solomon <148258001+abnsol@users.noreply.github.com>
2025-05-29 00:44:35 +0300

    Created 'Time Needed to Inform All Employees' by A2SV Hub

---
+++ 28-May-2025/Time Needed to Inform All Employees 360638.py
@@ -0,0 +1,15 @@
+# Problem: Time Needed to Inform All Employees - https://leetcode.com/problems/time-needed-to-inform-all-employees/
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        subordinates = collections.defaultdict(list)
+        self.res = 0
+        for i, v in enumerate(manager):
+            subordinates[v].append(i)
+        
+        def dfs(manager, time):
+            self.res = max(self.res, time)
+            for subordinate in subordinates[manager]:
+                dfs(subordinate, time + informTime[manager])
+        dfs(headID, 0)        
+        return self.res
